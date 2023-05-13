e5d191cde28e26d66b446869ef74a438303e1aaf
rodion56730 <rodion56730@gmail.com>
2023-05-13 14:35:07 +0300

    1

--- app/src/test/java/com/gorchatov/shazamclone/shazamclone/MusicDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.gorchatov.shazamclone.shazamclone;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Music.class}, version = 5)
-public abstract class MusicDatabase extends RoomDatabase {
-    public abstract MusicDao musicDao();
-}
